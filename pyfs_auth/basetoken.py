675f63a0e1af82681b0f0a2757555d0812616e94
huangqimin001 <huangqimin001@ke.com>
2020-09-19 03:38:20 +0800

    :art: Update tenant_access_token get by tenant_key

--- pyfs_auth/basetoken.py
+++ pyfs_auth/basetoken.py
@@ -7,11 +7,12 @@ from pywe_storage import MemoryStorage
 
 
 class BaseToken(BaseFeishu):
-    def __init__(self, appid=None, secret=None, ticket=None, token=None, storage=None, token_fetched_func=None, refresh_left_seconds=6600, token_type=''):
+    def __init__(self, appid=None, secret=None, ticket=None, tenant_key=None, token=None, storage=None, token_fetched_func=None, refresh_left_seconds=6600, token_type=''):
         super(BaseToken, self).__init__()
         self.appid = appid
         self.secret = secret
         self.ticket = ticket
+        self.tenant_key = tenant_key
         self.token = token
         self.storage = storage or MemoryStorage()
         self.token_fetched_func = token_fetched_func
@@ -31,10 +32,11 @@ class BaseToken(BaseFeishu):
     def access_info_key(self):
         return 'feishu:{0}:{1}:info'.format(self.appid, self.token_type)
 
-    def update_params(self, appid=None, secret=None, ticket=None, token=None, storage=None, token_fetched_func=None, refresh_left_seconds=6600, token_type=''):
+    def update_params(self, appid=None, secret=None, ticket=None, tenant_key=None, token=None, storage=None, token_fetched_func=None, refresh_left_seconds=6600, token_type=''):
         self.appid = appid or self.appid
         self.secret = secret or self.secret
         self.ticket = ticket or self.ticket
+        self.tenant_key = tenant_key or self.tenant_key
         self.token = token or self.token
         self.storage = storage or self.storage
         self.token_fetched_func = token_fetched_func or self.token_fetched_func
--- pyfs_auth/tenant_access_token.py
+++ pyfs_auth/tenant_access_token.py
@@ -4,23 +4,24 @@ import time
 
 from pywe_exception import WeChatException
 
-from .basetoken import BaseToken
+from app_access_token import AppAccessToken, final_app_access_token
 
 
-class TenantAccessToken(BaseToken):
-    def __init__(self, appid=None, secret=None, ticket=None, token=None, storage=None, token_fetched_func=None, refresh_left_seconds=6600):
-        super(TenantAccessToken, self).__init__(appid=appid, secret=secret, ticket=ticket, token=token, storage=storage, token_fetched_func=token_fetched_func, refresh_left_seconds=refresh_left_seconds, token_type='tenant_access_token')
+class TenantAccessToken(AppAccessToken):
+    def __init__(self, appid=None, secret=None, ticket=None, tenant_key=None, token=None, storage=None, token_fetched_func=None, refresh_left_seconds=6600):
+        super(TenantAccessToken, self).__init__(appid=appid, secret=secret, ticket=ticket, tenant_key=tenant_key, token=token, storage=storage, token_fetched_func=token_fetched_func, refresh_left_seconds=refresh_left_seconds, token_type='tenant_access_token')
         # 获取 tenant_access_token（应用商店应用）, Refer: https://open.feishu.cn/document/ukTMukTMukTM/uMjNz4yM2MjLzYzM
         self.TENANT_ACCESS_TOKEN = self.OPEN_DOMAIN + '/open-apis/auth/v3/tenant_access_token/'
 
     def __about_to_expires(self, expires_at, refresh_left_seconds=6600):
         return expires_at and expires_at - int(time.time()) < refresh_left_seconds
 
-    def __fetch_access_token(self, appid=None, secret=None, ticket=None, storage=None, token_fetched_func=None, refresh_left_seconds=6600):
+    def __fetch_access_token(self, appid=None, secret=None, ticket=None, tenant_key=None, storage=None, token_fetched_func=None, refresh_left_seconds=6600):
         # Update Params
-        self.update_params(appid=appid, secret=secret, ticket=ticket, storage=storage, token_fetched_func=token_fetched_func, refresh_left_seconds=refresh_left_seconds)
+        self.update_params(appid=appid, secret=secret, ticket=ticket, tenant_key=tenant_key, storage=storage, token_fetched_func=token_fetched_func, refresh_left_seconds=refresh_left_seconds)
         # Access Info Request
-        access_info = self.post(self.TENANT_ACCESS_TOKEN, data={'app_id': self.appid, 'app_secret': self.secret, 'app_ticket': self.ticket})
+        app_access_token = final_app_access_token(self, appid=self.appid, secret=self.secret, ticket=self.ticket, storage=self.storage)
+        access_info = self.post(self.TENANT_ACCESS_TOKEN, data={'app_access_token': app_access_token, 'tenant_key': tenant_key}, authorization=False)
         # Request Error
         if 'expire' not in access_info:
             raise WeChatException(access_info)
@@ -34,22 +35,22 @@ class TenantAccessToken(BaseToken):
         # Return Access Token
         return access_info.get('tenant_access_token')
 
-    def access_token(self, appid=None, secret=None, ticket=None, storage=None, token_fetched_func=None, refresh_left_seconds=6600):
+    def access_token(self, appid=None, secret=None, ticket=None, tenant_key=None, storage=None, token_fetched_func=None, refresh_left_seconds=6600):
         # Update Params
-        self.update_params(appid=appid, secret=secret, ticket=ticket, storage=storage, token_fetched_func=token_fetched_func, refresh_left_seconds=refresh_left_seconds)
+        self.update_params(appid=appid, secret=secret, ticket=ticket, tenant_key=tenant_key, storage=storage, token_fetched_func=token_fetched_func, refresh_left_seconds=refresh_left_seconds)
         # Fetch access_info
         access_info = self.storage.get(self.access_info_key)
         if access_info:
             access_token = access_info.get('tenant_access_token')
             if access_token and not self.__about_to_expires(access_info.get('expires_at'), refresh_left_seconds=refresh_left_seconds):
                 return access_token
-        return self.__fetch_access_token(self.appid, self.secret, self.ticket, self.storage, token_fetched_func=self.token_fetched_func, refresh_left_seconds=refresh_left_seconds)
+        return self.__fetch_access_token(self.appid, self.secret, self.ticket, self.tenant_key, self.storage, token_fetched_func=self.token_fetched_func, refresh_left_seconds=refresh_left_seconds)
 
-    def refresh_access_token(self, appid=None, secret=None, ticket=None, storage=None, token_fetched_func=None, refresh_left_seconds=6600):
-        return self.__fetch_access_token(appid, secret, ticket, storage, token_fetched_func=token_fetched_func, refresh_left_seconds=refresh_left_seconds)
+    def refresh_access_token(self, appid=None, secret=None, ticket=None, tenant_key=None, storage=None, token_fetched_func=None, refresh_left_seconds=6600):
+        return self.__fetch_access_token(appid, secret, ticket, tenant_key, storage, token_fetched_func=token_fetched_func, refresh_left_seconds=refresh_left_seconds)
 
-    def final_access_token(self, cls=None, appid=None, secret=None, ticket=None, token=None, storage=None, token_fetched_func=None, refresh_left_seconds=6600):
-        return token or self.access_token(appid or cls.appid, secret or cls.secret, ticket or cls.ticket, storage=storage or cls.storage, token_fetched_func=token_fetched_func or cls.token_fetched_func, refresh_left_seconds=refresh_left_seconds or cls.refresh_left_seconds)
+    def final_access_token(self, cls=None, appid=None, secret=None, ticket=None, tenant_key=None, token=None, storage=None, token_fetched_func=None, refresh_left_seconds=6600):
+        return token or self.access_token(appid or cls.appid, secret or cls.secret, ticket or cls.ticket, tenant_key or cls.tenant_key, storage=storage or cls.storage, token_fetched_func=token_fetched_func or cls.token_fetched_func, refresh_left_seconds=refresh_left_seconds or cls.refresh_left_seconds)
 
 
 token = TenantAccessToken()
