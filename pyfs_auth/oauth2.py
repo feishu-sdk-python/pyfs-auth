876995f36eca975e8226e91eca69198d1c97d923
huangqimin001 <huangqimin001@ke.com>
2021-03-25 02:12:22 +0800

    :art: Support client oauth2

--- pyfs_auth/oauth2.py
+++ pyfs_auth/oauth2.py
@@ -1,30 +1,29 @@
 # -*- coding: utf-8 -*-
 
 from pyfs_base import BaseFeishu
-from .app_access_token import AppAccessToken, final_app_access_token
 
 
 class OAuth2(BaseFeishu):
-    def __init__(self, appid=None, secret=None, ticket=None, tenant_key=None, token=None, storage=None):
-        super(Authen, self).__init__(appid=appid, secret=secret, ticket=ticket, tenant_key=tenant_key, token=token, storage=storage)
+    def __init__(self):
+        super(OAuth2, self).__init__()
         # 移动应用接入 - 接入流程, Refer: https://open.feishu.cn/document/uAjLw4CM/uYjL24iN/mobile-app/mobile-app-overview
         # 第二步：获取 access_token
         self.SUITE_PASSPORT_OAUTH_TOKEN = self.PASSPORT_DOMAIN + '/suite/passport/oauth/token'
         # 第三步：获取用户信息
         self.SUITE_PASSPORT_OAUTH_USERINFO = self.PASSPORT_DOMAIN + '/suite/passport/oauth/userinfo'
 
-    def get_access_info(self, code=None, grant_type='authorization_code', appid=None, secret=None, code_verifier=None):
+    def get_access_info(self, code=None, grant_type='authorization_code', appid=None, client_id=None, secret=None, client_secret=None, code_verifier=None):
         return self.post(self.SUITE_PASSPORT_OAUTH_TOKEN, data={
             'grant_type': grant_type,
-            'client_id': appid,
-            'client_secret': secret,
+            'client_id': client_id or appid,
+            'client_secret': client_secret or secret,
             'code': code,
             'code_verifier': code_verifier,
         }, content_type='application/x-www-form-urlencoded')
 
-    def get_userinfo(self, access_token=None, code=None, grant_type='authorization_code', appid=None, secret=None, code_verifier=None):
+    def get_userinfo(self, access_token=None, code=None, grant_type='authorization_code', appid=None, client_id=None, secret=None, client_secret=None, code_verifier=None):
         if not access_token:
-            access_token = self.get_access_info(code=code, grant_type=grant_type, appid=appid, secret=secret, code_verifie=code_verifier)
+            access_token = self.get_access_info(code=code, grant_type=grant_type, appid=appid, client_id=client_id, secret=secret, client_secret=client_secret, code_verifie=code_verifier).get('access_token')
         return self.post(self.SUITE_PASSPORT_OAUTH_USERINFO, data={
             'token': access_token,
         })
